2fc33f00b8a21011369b17749445365d088bc459
Per Wikman <perwikman89@gmail.com>
2017-05-06 16:30:17 +0200

    add merge function

--- Program/7_diagnoses.R
+++ Program/7_diagnoses.R
@@ -11,13 +11,13 @@ if(BARN){
   par_barn <- readRDS("Output/2_par_barn.rds")
  
   setDT(par_barn)
-  par_barn[,(metadata$variable):=lapply(metadata$search, applySearch, variable = par_barn$DIAGNOS),]
+  par_barn[,(paste0(metadata$variable, "_parbarn")):=lapply(metadata$search, applySearch, variable = par_barn$DIAGNOS),]
   # derive new diagnoses
  
   par_barn <- par_barn[,lapply(.SD, function(x){ifelse(sum(x, na.rm = TRUE)>1,1,0)}), by = "lopnr", .SDcols = metadata$variable]
   
   par_barn <- data.frame(par_barn)
-  saveRDS("Output/7_par_barn.rds")
+  saveRDS(par_barn, "Output/7_par_barn.rds")
 }
 
 #------------------------------- PAR FORALDRAR ---------------------------------
@@ -32,14 +32,28 @@ if(MFR){
   
   
   setDT(mfr)
-  mfr[,(metadata$variable):=lapply(metadata$search, applySearch, variable = mfr$BDIAGS),]
+  mfr[,(paste0(metadata$variable, "_mfr")):=lapply(metadata$search, applySearch, variable = mfr$BDIAGS),]
   # derive new diagnoses
   
   mfr <- mfr[,lapply(.SD, function(x){ifelse(sum(x, na.rm = TRUE)>1,1,0)}), by = "BLOPNR", .SDcols = metadata$variable]
   
   mfr <- data.frame(mfr)
-  saveRDS("Output/7_mfr.rds")
+  saveRDS(mfr, "Output/7_mfr.rds")
 }
+#------------------ Merge (maybe move this to another script) ------------------
+
+if(MERGE){
+  mfr <- readRDS("Output/7_mfr.rds")
+  par_barn <- readRDS("Output/7_par_barn.rds")
+  
+  par_barn <- dplyr::rename(par_barn, BLOPNR = lopnr)
+  
+  
+  m
+
+}
+#-------------------------------------------------------------------------------
+
 #-------------------------------------------------------------------------------
  # calculate derived vars
 # for( i in 1:nrow(derivVars)){
