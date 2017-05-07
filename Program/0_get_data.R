293aa0b4f94f67734cf5b8757da8b171c5a47166
Per Wikman <perwikman89@gmail.com>
2017-05-07 13:36:34 +0200

    merge child sources

--- Program/0_get_data.R
+++ Program/0_get_data.R
@@ -1,13 +1,15 @@
 rm(list=objects())
-
+library(data.table)
+library(dplyr)
+library(tidyr)
 #-------------------------------------------------------------------------------
 
 
 #Source
-source("Program/1_mfr.R", encoding="utf-8")
-source("Program/2_par_barn.R", encoding="utf-8")
-source("Program/3_par_foralder.R", encoding="utf-8")
-source("Program/7_diagnoses.R", encoding="utf-8")
+system.time({source("Program/1_mfr.R", encoding="utf-8")})
+system.time({source("Program/2_par_barn.R", encoding="utf-8")})
+system.time({source("Program/3_par_foralder.R", encoding="utf-8")})
+system.time({source("Program/7_diagnoses.R", encoding="utf-8")})
 
 
 
--- Program/7_diagnoses.R
+++ Program/7_diagnoses.R
@@ -4,6 +4,7 @@ source("Program/functions.R", encoding = "utf-8")
 BARN = TRUE
 FORALDRAR = FALSE
 MFR = TRUE
+MERGE = FALSE
  
 # derive diagnoses in steps. Apply search per source 
 #--------------------------------- PAR BARN ------------------------------------
@@ -14,10 +15,12 @@ if(BARN){
   par_barn[,(paste0(metadata$variable, "_parbarn")):=lapply(metadata$search, applySearch, variable = par_barn$DIAGNOS),]
   # derive new diagnoses
  
-  par_barn <- par_barn[,lapply(.SD, function(x){ifelse(sum(x, na.rm = TRUE)>1,1,0)}), by = "lopnr", .SDcols = metadata$variable]
+  par_barn <- par_barn[,lapply(.SD, function(x){ifelse(sum(x, na.rm = TRUE)>1,1,0)}), by = "lopnr", .SDcols = paste0(metadata$variable, "_parbarn")]
   
   par_barn <- data.frame(par_barn)
   saveRDS(par_barn, "Output/7_par_barn.rds")
+  rm(par_barn)
+  gc()
 }
 
 #------------------------------- PAR FORALDRAR ---------------------------------
@@ -32,13 +35,15 @@ if(MFR){
   
   
   setDT(mfr)
-  mfr[,(paste0(metadata$variable, "_mfr")):=lapply(metadata$search, applySearch, variable = mfr$BDIAGS),]
+  mfr[,(paste0(metadata$variable, "_mfr")):=lapply(metadata$search, applySearch, variable = mfr$BDIAG),]
   # derive new diagnoses
   
-  mfr <- mfr[,lapply(.SD, function(x){ifelse(sum(x, na.rm = TRUE)>1,1,0)}), by = "BLOPNR", .SDcols = metadata$variable]
+  mfr <- mfr[,lapply(.SD, function(x){ifelse(sum(x, na.rm = TRUE)>1,1,0)}), by = "BLOPNR", .SDcols = paste0(metadata$variable, "_mfr")]
   
   mfr <- data.frame(mfr)
   saveRDS(mfr, "Output/7_mfr.rds")
+  rm(mfr)
+  gc()
 }
 #------------------ Merge (maybe move this to another script) ------------------
 
@@ -48,8 +53,77 @@ if(MERGE){
   
   par_barn <- dplyr::rename(par_barn, BLOPNR = lopnr)
   
+  setDT(mfr, key = "BLOPNR")
+  setDT(par_barn, key = "BLOPNR")
   
-  m
+    
+  analysdata <- merge(mfr, par_barn, by = "BLOPNR", all.x = TRUE)
+  
+  fall_kontroll <- read.table("Indata/Sos_20170407/SoS/Data/META_28574_2015.tab",
+             sep = "\t",
+             header = TRUE,
+             stringsAsFactors = FALSE
+  )
+  
+  fall_kontroll <- fall_kontroll %>% 
+    select(LopNr, TYPE, FODAR) %>% 
+    rename(BLOPNR = LopNr) %>% 
+    setDT(key = "BLOPNR") %>% 
+    distinct()
+  
+  
+  
+  
+  analysdata <- merge(analysdata, fall_kontroll, by = "BLOPNR", all.x = TRUE)
+  
+  analysdata[,(metadata$variable):=lapply(metadata$variable, 
+                                          function(x){
+                                            
+                                            sumvars <- grep(paste0("^", x ,"_"), names(analysdata), value = TRUE)
+                                            
+                                            if(length(sumvars) != 2){
+                                              stop("Incorrect number of variables to sum")
+                                            }else{
+                                            ifelse(rowSums(analysdata[,sumvars, with = FALSE], na.rm = TRUE) > 0, 1, 0)
+                                            }
+                                          }),
+             ]
+  
+  # these should all be the same
+  #table(analysdata$n_maltreatmentSyndrome == analysdata$n_maltreatmentSyndrome_mfr + analysdata$n_maltreatmentSyndrome_parbarn) 
+
+  tmp <- grep("_mfr$|_parbarn$", names(analysdata), value = TRUE, invert = TRUE)
+  analysdata <- analysdata[,tmp, with = FALSE]
+  
+  n_table <-   
+    analysdata %>% 
+    group_by(TYPE, FODAR) %>% 
+    summarise(n = n())
+  
+  out <- 
+  analysdata %>% 
+    select(-BLOPNR) %>% 
+    group_by(TYPE, FODAR) %>%
+    summarise_each(funs(sum)) %>% 
+    data.frame %>% 
+    arrange(FODAR, TYPE)
+  
+#  analysdata_long <-
+#    analysdata %>%
+#    select(-BLOPNR)%>%
+#    gather(key, value, -TYPE, -FODAR)
+  
+  out <- merge(out, n_table, by = c("TYPE", "FODAR"))
+  
+  out %>% 
+    gather(variabel, antal, -TYPE, -FODAR, -n) %>% 
+    mutate(per_100000 = round(100000 * antal/n, 2)) %>%
+    gather(key, value, -TYPE, -FODAR, -n, -variabel) %>% 
+    mutate(variabel = paste0(variabel, "_", key)) %>% 
+    select(-key) %>% 
+    spread(variabel, value) %>% 
+    arrange(FODAR, TYPE) %>% 
+    openxlsx::write.xlsx("Output/7_barndiagnoser.xlsx")
 
 }
 #-------------------------------------------------------------------------------
